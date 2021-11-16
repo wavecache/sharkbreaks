27ee2e1844b5a7c9cba8a46dc87d073ef8479249
Lasse Tjernæs Wardenær <lasse.wardenar@gmail.com>
2021-11-15 23:36:49 -1000

    Add sub menu

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -9,39 +9,74 @@ import { Roles } from 'meteor/alanning:roles';
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
+    const blueColor = '#2196f3';
     const menuStyle = {
       marginBottom: '10px',
-      backgroundColor: '#2196f3',
+      backgroundColor: blueColor,
+    };
+    const subMenuStyle = {
+      boxShadow: 'none',
+      border: 'none',
+      backgroundColor: 'white',
+      marginBottom: '10px',
+      display: 'flex',
+      justifyContent: 'center',
+    };
+    const subMenuItemStyle = {
+      color: blueColor,
+      paddingRight: '2rem',
+      paddingLeft: '2rem',
     };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
-        <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>Surf Breaks</Header>
-        </Menu.Item>
-        {this.props.currentUser ? (
-          [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-        ) : ''}
-        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-        ) : ''}
-        <Menu.Item position="right">
-          {this.props.currentUser === '' ? (
-            <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Menu.Item>
-      </Menu>
+      <div>
+        <Menu style={menuStyle} attached="top" borderless inverted>
+          <Menu.Item as={NavLink} activeClassName="" exact to="/">
+            <Header inverted as='h1'>Surf Breaks</Header>
+          </Menu.Item>
+          {this.props.currentUser ? (
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+          ) : ''}
+          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+          ) : ''}
+          <Menu.Item position="right">
+            {this.props.currentUser === '' ? (
+              <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
+                <Dropdown.Menu>
+                  <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
+                  <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
+                <Dropdown.Menu>
+                  <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+          </Menu.Item>
+        </Menu>
+        <Menu style={subMenuStyle} borderless attached="top" fluid secondary>
+          <Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/" position='left'>
+              <Header style={subMenuItemStyle} as='h3'>Find Your Break</Header>
+            </Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/">
+              <Header style={subMenuItemStyle} as='h3'>Surfing Conditions</Header>
+            </Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/">
+              <Header style={subMenuItemStyle} as='h3'>Friends List</Header>
+            </Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/">
+              <Header style={subMenuItemStyle} as='h3'>Forum</Header>
+            </Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/" position='right'>
+              <Header style={subMenuItemStyle} as='h3'>Your Profile</Header>
+            </Menu.Item>
+          </Menu.Item>
+        </Menu>
+      </div>
     );
   }
 }
