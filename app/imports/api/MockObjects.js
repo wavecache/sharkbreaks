73fa2cb73e2bb17cefb8e270effded0129f24f73
Lasse Tjernæs Wardenær <lasse.wardenar@gmail.com>
2021-11-17 00:28:26 -1000

    Add testing data

--- app/imports/api/MockObjects.js
+++ app/imports/api/MockObjects.js
@@ -7,3 +7,27 @@ export const surfBreakMockObject = {
   description: 'If you are on a quest to find the legendary Banzai Pipeline Hawaii, known around the world  for its perfect barreling waves, then head to Ehukai Beach Park on the North Shore Oahu.',
   address: '59-337 Ke Nui Rd, Haleiwa, HI 96712',
 };
+
+const surfBreakMockObject2 = {
+  name: 'Banzai Pipeline',
+  followersIds: ['ksdnfksfn', 'sldknflskdnf', 'sdlkfnvosdfngo'],
+  image: 'https://lushpalm.com/wp-content/uploads/2017/11/oahu-surf-spots-pipeline.webp',
+  description: 'If you are on a quest to find the legendary Banzai Pipeline Hawaii, known around the world  for its perfect barreling waves, then head to Ehukai Beach Park on the North Shore Oahu.',
+  address: '59-337 Ke Nui Rd, Haleiwa, HI 96712',
+};
+
+const surfBreakMockObject3 = {
+  name: 'Banzai Pipeline',
+  followersIds: ['sldknflskdnf', 'sdlkfnvosdfngo'],
+  image: 'https://lushpalm.com/wp-content/uploads/2017/11/oahu-surf-spots-pipeline.webp',
+  description: 'If you are on a quest to find the legendary Banzai Pipeline Hawaii, known around the world  for its perfect barreling waves, then head to Ehukai Beach Park on the North Shore Oahu.',
+  address: '59-337 Ke Nui Rd, Haleiwa, HI 96712',
+};
+
+/* ----------- SurfBreaks page ----------- */
+
+export const surfBreakMockObjects = [
+  surfBreakMockObject,
+  surfBreakMockObject2,
+  surfBreakMockObject3,
+];
