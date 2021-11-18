c5f4f77cbba27c8c6714fe69b027b9f38905e42e
Chasem6 <77372356+Chasem6@users.noreply.github.com>
2021-11-17 14:26:06 -1000

    add ListFriends
    with a card for chicken joe

---
+++ app/imports/ui/components/Friend.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Friend extends React.Component {
+  render() {
+    return (
+      <Card>
+        <Image src={this.props.friend.image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{this.props.friend.firstName} {this.props.friend.lastName}</Card.Header>
+          <Card.Meta>
+            <span className='date'>{this.props.friend.level}</span>
+          </Card.Meta>
+          <Card.Description>
+            {this.props.friend.bio}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+Friend.propTypes = {
+  friend: PropTypes.object.isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(Friend);
---
+++ app/imports/ui/pages/ListFriends.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Card, Header, Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Stuffs } from '../../api/stuff/Stuff';
+import Friend from '../components/Friend';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ListFriends extends React.Component {
+
+  contacts = [{
+    firstName: 'Chicken', lastName: 'Joe', level: 'Advanced',
+    image: 'https://m.media-amazon.com/images/I/51e3smIL9bL._AC_SL1000_.jpg',
+    bio: 'Eh, no drop in on me brah',
+  },
+  ];// If the subscription(s) have been received, render the page, otherwise show a loading icon.
+
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the page once subscriptions have been received.
+  renderPage() {
+    return (
+      <Container>
+        <Header as="h2" textAlign="center">Friends</Header>
+        <Card.Group>
+          {this.contacts.map((friend, index) => <Friend key={index} friend={friend} />)}
+        </Card.Group>
+      </Container>
+    );
+  }
+}
+
+// Require an array of Stuff documents in the props.
+ListFriends.propTypes = {
+  friends: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const stuffs = Stuffs.collection.find({}).fetch();
+  return {
+    stuffs,
+    ready,
+  };
+})(ListFriends);
+
+// need to add to app.jsx, maybe to navbar?
+// name, level, bio... etc?
