bea489bdf2ef800634e048b77fb2f705808ad177
quinnbl <quinnbl@hawaii.edu>
2021-11-18 20:44:36 -1000

    Finished first draft of landing page and updated navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,54 +3,52 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header, Container } from 'semantic-ui-react';
+import { Menu, Dropdown, Header, Container, Image } from 'semantic-ui-react';
 import '../../../client/style.css';
-import { menuStyle, subMenuStyle, subMenuItemStyle } from '../layouts/style';
+import { subMenuStyle, subMenuItemStyle } from '../layouts/style';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Menu style={menuStyle} attached="top" borderless inverted>
-          <Menu.Item as={NavLink} activeClassName="" exact to="/">
-            <Header inverted as='h1'>WaveCache</Header>
-          </Menu.Item>
-          <Menu.Item position="right">
-            {this.props.currentUser === '' ? (
-              <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
-                <Dropdown.Menu>
-                  <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                  <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
-                </Dropdown.Menu>
-              </Dropdown>
-            ) : (
-              <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
-                <Dropdown.Menu>
-                  <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-                </Dropdown.Menu>
-              </Dropdown>
-            )}
-          </Menu.Item>
-        </Menu>
         <Container>
           <Menu style={subMenuStyle} borderless attached="top" fluid secondary>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/" position='left'>
+              <Image src='/images/meteor-logo.png' size='small'/>
+            </Menu.Item>
             <Menu.Item as={NavLink} activeClassName="" exact to="/surfBreaks">
-              <Header style={subMenuItemStyle} as='h3'>Find Your Break</Header>
+              <Header style={subMenuItemStyle} as='h2'>Find Your Break</Header>
             </Menu.Item>
             <Menu.Item as={NavLink} activeClassName="" exact to="/">
-              <Header style={subMenuItemStyle} as='h3'>Forum</Header>
+              <Header style={subMenuItemStyle} as='h2'>Forum</Header>
             </Menu.Item>
             {this.props.currentUser === '' ? null : (
               <Menu.Item as={NavLink} activeClassName="" exact to="/list">
-                <Header style={subMenuItemStyle} as='h3'>Friends List</Header>
+                <Header style={subMenuItemStyle} as='h2'>Friends List</Header>
               </Menu.Item>
             )}
             {this.props.currentUser === '' ? null : (
               <Menu.Item as={NavLink} activeClassName="" exact to="/">
-                <Header style={subMenuItemStyle} as='h3'>Your Profile</Header>
+                <Header style={subMenuItemStyle} as='h2'>Your Profile</Header>
               </Menu.Item>
             )}
+            <Menu.Item position="right">
+              {this.props.currentUser === '' ? (
+                <Dropdown style={subMenuItemStyle} id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item style={subMenuItemStyle} id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
+                    <Dropdown.Item style={subMenuItemStyle} id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : (
+                <Dropdown style={subMenuItemStyle} id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item style={subMenuItemStyle} id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
+            </Menu.Item>
           </Menu>
         </Container>
       </div>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
+import { landingHeader } from '../layouts/style';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <div style={{ backgroundImage: 'url(./images/Images/RockyPoint.jpg)',
+        backgroundSize: 'cover',
+        height: '700px',
+        paddingTop: '300px',
+        paddingRight: '400px',
+      }}>
+        <Grid id='landing-page' verticalAlign='center' textAlign='center' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
+          <Grid.Column width={7} floated = 'left'>
+            <Header style={landingHeader} as='h1' inverted>Wave Cache</Header>
+            <Header as='h3' inverted>
+              Hello Surfing World! And welcome to Wave Cache, the place for you to cache all the gnarliest waves.
+              We have compiled a list of our favorites, with some killer info and weather reports. All we need is
+              for you to use all of it to make some friends, be safe and shred some waves!
+            </Header>
+          </Grid.Column>
 
-      </Grid>
+        </Grid>
+      </div>
     );
   }
 }
