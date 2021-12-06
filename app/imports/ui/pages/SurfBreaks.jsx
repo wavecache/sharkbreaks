7bb506c7607f174a685872b457f08e179ce367d4
Lasse Tjernæs Wardenær <lasse.wardenar@gmail.com>
2021-12-06 00:26:50 -1000

    Filtering works

--- app/imports/ui/pages/SurfBreaks.jsx
+++ app/imports/ui/pages/SurfBreaks.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Button, CardGroup, Container, Header, Loader } from 'semantic-ui-react';
+import { Button, CardGroup, Container, Header, Input, Loader, Search } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
 import { blueTextStyle, buttonStyle } from '../layouts/style';
 import SurfBreak from '../components/SurfBreak';
 import { SurfBreakData } from '../../api/surfbreak/SurfBreakData';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class SurfBreaks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentSurfBreaks: [] };
+    this.initialValue = true;
+  }
+
+  filterSurfBreaks(surfBreaksSearch, allSurfBreaks) {
+    if (surfBreaksSearch === '') {
+      this.setState(() => ({ currentSurfBreaks: allSurfBreaks }));
+      return;
+    }
+    this.setState(() => {
+      const filteredBreaks = _.filter(allSurfBreaks, (surfBreak) => {
+        const searchLength = surfBreaksSearch.length;
+        return surfBreak.name.substr(0, searchLength) === surfBreaksSearch;
+      });
+      console.log(filteredBreaks);
+      return { currentSurfBreaks: filteredBreaks };
+    });
+  }
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -17,11 +38,20 @@ class SurfBreaks extends React.Component {
   }
 
   renderPage() {
+    let renderSurfBreaks = [];
+    if (this.state.currentSurfBreaks.length === 0 && this.initialValue) {
+      console.log('Hello')
+      this.initialValue = false;
+      renderSurfBreaks = this.props.surfBreaks;
+    } else {
+      renderSurfBreaks = this.state.currentSurfBreaks;
+    }
     return (
       <Container id='surfBreaks-page'>
         <Header style={ blueTextStyle } as="h2" textAlign="center">Surf Breaks</Header>
+        <Input placeholder='' onChange={(surfBreaks) => this.filterSurfBreaks(surfBreaks.target.value, this.props.surfBreaks)}/>
         <CardGroup centered>
-          {this.props.surfBreaks.map((surfBreak, index) => <SurfBreak id={`surfBreak${index}`} key={index} surfBreak={surfBreak} currentUser={this.props.currentUser}/>)}
+          {renderSurfBreaks.map((surfBreak, index) => <SurfBreak id={`surfBreak${index}`} key={index} surfBreak={surfBreak} currentUser={this.props.currentUser}/>)}
         </CardGroup>
         {this.props.currentUser === '' ? null : (
           <Link to={'/addSurfBreak'} style={{ display: 'flex', justifyContent: 'center' }}>
