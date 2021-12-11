58ace130a7d674ffd816e1d59feb213f3c27032e
Lasse Tjernæs Wardenær <lasse.wardenar@gmail.com>
2021-12-10 17:50:59 -1000

    Add filter surf breaks test

--- app/imports/ui/pages/SurfBreaks.jsx
+++ app/imports/ui/pages/SurfBreaks.jsx
@@ -47,7 +47,7 @@ class SurfBreaks extends React.Component {
     return (
       <Container id='surfBreaks-page'>
         <Header style={ blueTextStyle } as="h2" textAlign="center">Surf Breaks</Header>
-        <Input placeholder='' size='big' style={searchBarStyle} label='Filter' onChange={(surfBreaks) => this.filterSurfBreaks(surfBreaks.target.value, this.props.surfBreaks)}/>
+        <Input id='filter-breaks' placeholder='' size='big' style={searchBarStyle} label='Filter' onChange={(surfBreaks) => this.filterSurfBreaks(surfBreaks.target.value, this.props.surfBreaks)}/>
         <CardGroup centered>
           {renderSurfBreaks.map((surfBreak, index) => <SurfBreak surfBreakPage={this} id={`surfBreak${index}`} key={index} surfBreak={surfBreak} currentUser={this.props.currentUser}/>)}
         </CardGroup>
